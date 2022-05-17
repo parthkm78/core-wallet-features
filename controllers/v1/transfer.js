3214013646fabf169e8fe650e3ccddba38542e1c
Parth <parthkm78@gmail.com>
2022-05-17 17:30:00 +0530

    balance check added

--- controllers/v1/transfer.js
+++ controllers/v1/transfer.js
@@ -37,11 +37,16 @@ const sendETH = async (req, res) => {
 
     // fetch sender balance
     const valBalance = await walletSigner.getBalance()
-    console.log("baancce", valBalance)
+    logger.info("from bal:", ethers.utils.formatUnits(valBalance, "ether"))
 
     const tobal = await provider.getBalance(toAddress)
-    console.log("baancce", valBalance)
+    logger.info("to bal:", ethers.utils.formatUnits(tobal, "ether"))
 
+    if(await ethers.utils.formatUnits(valBalance, "ether") <= amount )
+    {
+      return res.status(HTTP_STATUS_CODE.BAD_REQUEST)
+      .json(errorResponse(ERROR_MESSAGE.INSUFFICIENT_BALANCE));
+    }
 
     logger.info(`sending ${amount} from ${wallet.address} to ${toAddress}`)
 
@@ -64,7 +69,7 @@ const sendETH = async (req, res) => {
       .json(successResponse("ETH send sucessfuly", { transectionHash : transection.hash}));
   }
   catch (err) {
-   console.log("Internal server error:", err);
+   logger.error("Internal server error:", err);
     return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER)
       .json(errorResponse(ERROR_MESSAGE.INTERNAL_SERVER_ERROR, 
         { transectionDetail : { transectionHash :  err?.transactionHash,
@@ -77,6 +82,7 @@ const sendBone = async (req, res) => {
   let transection;
   try {
     const toAddress = req.body.toAddress;
+    // ** This will be removed *******
     const privateKey = req.body.privateKey;
     const amount = req.body.amount;
     // check requets has required parameters
@@ -97,12 +103,16 @@ const sendBone = async (req, res) => {
 
     // fetch sender balance
     const valBalance = await walletSigner.getBalance()
-    console.log("baancce", valBalance)
+    logger.info("from bal"+ await ethers.utils.formatUnits(valBalance, "ether"))
 
     const tobal = await provider.getBalance(toAddress)
-    console.log("baancce", valBalance)
+    logger.info("to bal" +  await ethers.utils.formatUnits(tobal, "ether"))
 
-
+    if(await ethers.utils.formatUnits(valBalance, "ether") <= amount )
+    {
+      return res.status(HTTP_STATUS_CODE.BAD_REQUEST)
+      .json(errorResponse(ERROR_MESSAGE.INSUFFICIENT_BALANCE));0/transfer/eth
+    }
     logger.info(`sending ${amount} from ${wallet.address} to ${toAddress}`)
 
     const gasPrice = await provider.getGasPrice()
@@ -121,10 +131,10 @@ const sendBone = async (req, res) => {
   await transection.wait()
 
   res.status(HTTP_STATUS_CODE.OK)
-      .json(successResponse("ETH send sucessfuly", { transectionHash : transection.hash}));
+      .json(successResponse("BONE send sucessfuly", { transectionHash : transection.hash}));
   }
   catch (err) {
-   console.log("Internal server error:", err);
+   logger.error("Internal server error:", err);
     return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER)
       .json(errorResponse(ERROR_MESSAGE.INTERNAL_SERVER_ERROR, 
         { transectionDetail : { transectionHash :  err?.transactionHash,
