38d15b30703c9142e4aede52c0c16d372f729ed5
Anshul Goyal <ag91097@gmail.com>
2022-05-18 09:55:00 +0530

    Minor tokenInfo API changes

--- controllers/v1/token.js
+++ controllers/v1/token.js
@@ -1,33 +1,31 @@
+require("dotenv").config();
 const { errorResponse, successResponse } = require("../../util/response.js");
 const { ERROR_MESSAGE, HTTP_STATUS_CODE } = require("../../util/constants.js");
 const logger = require("../../util/logger.js");
 const validator = require("express-validator");
 const { validationResult } = validator;
-
-require("dotenv").config();
 const { ethers } = require("ethers");
 const ChildERC20 = require("../../configs/contract/ChildERC20.json");
 
-const { CHILD_RPC_HTTP } = process.env;
-
-const provider = new ethers.providers.JsonRpcProvider(CHILD_RPC_HTTP);
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.CHILD_RPC_HTTP
+);
 
 const getInfo = async (req, res) => {
-  //   const token = req.query.tokenAddress;
   try {
-    const token = req.params.id;
+    const tokenAddress = req.params.tokenAddress;
 
-    logger.info(`Token Address ${token}`);
+    logger.info(`Token Address ${tokenAddress}`);
     const childERC20Contract = new ethers.Contract(
-      token,
+      tokenAddress,
       ChildERC20.abi,
       provider
     );
 
     const childChain = await childERC20Contract.childChain();
     const owner = await childERC20Contract.owner();
-    console.log("childChain", childChain);
-    console.log("owner", owner);
+    logger.info(`childChain, ${childChain}`);
+    logger.info(`owner, ${owner}`);
     return res.status(HTTP_STATUS_CODE.OK).json(
       successResponse("Token Info Fetched", {
         childChain: childChain,
@@ -35,7 +33,7 @@ const getInfo = async (req, res) => {
       })
     );
   } catch (error) {
-    console.log("Internal server error:", err);
+    logger.error(`Internal server error: , ${error}`);
     return res
       .status(HTTP_STATUS_CODE.INTERNAL_SERVER)
       .json(errorResponse(ERROR_MESSAGE.INTERNAL_SERVER));
--- routes/api/token.js
+++ routes/api/token.js
@@ -3,6 +3,6 @@ const tokenController = require("../../controllers/v1/token.js");
 const { errorResponse, successResponse } = require("../../util/response.js");
 const { ERROR_MESSAGE, HTTP_STATUS_CODE } = require("../../util/constants.js");
 
-router.get("/info/:id", tokenController.getInfo);
+router.get("/info/:tokenAddress", tokenController.getInfo);
 
 module.exports = router;
