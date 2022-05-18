3d01577a596c7b0818cbe43e15f0d5e6dbdec013
Anshul Goyal <ag91097@gmail.com>
2022-05-18 11:08:43 +0530

    Resolve server.js merge conflict

--- server.js
+++ server.js
@@ -45,6 +45,9 @@ app.use("/validator", validatorRouter);
 const balanceRouter = require("./routes/api/balance.js");
 app.use("/balance", balanceRouter);
 
+const transferRouter = require("./routes/api/transfer.js");
+app.use("/transfer", transferRouter);
+
 const tokenRouter = require("./routes/api/token.js");
 app.use("/token", tokenRouter);
 
