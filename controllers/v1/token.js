1da67d8b27f5488d5e8a71fb2ab23adbd08941ba
Anshul Goyal <ag91097@gmail.com>
2022-05-19 09:11:11 +0530

    Supported plasma/pos token api

--- controllers/v1/token.js
+++ controllers/v1/token.js
@@ -6,7 +6,8 @@ const validator = require("express-validator");
 const { validationResult } = validator;
 const { ethers } = require("ethers");
 const ChildERC20 = require("../../configs/contract/ChildERC20.json");
-
+const { getClient } = require("../../client/index.js");
+const config = require("../../util/networkConfig.js");
 const provider = new ethers.providers.JsonRpcProvider(
   process.env.CHILD_RPC_HTTP
 );
@@ -40,7 +41,149 @@ const getInfo = async (req, res) => {
   }
 };
 
+const getPlasma = async (req, res) => {
+  const client = await getClient("plasma");
+
+  const parentWeb3 = client.web3Client.getParentWeb3();
+  const abi = [
+    {
+      inputs: [],
+      name: "name",
+      outputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "symbol",
+      outputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
+
+  console.log("");
+  let tokens = new Array();
+
+  for (const [key, value] of Object.entries(config.tokens.plasma)) {
+    try {
+      const contract = new parentWeb3.eth.Contract(abi, value.parent);
+      const name = await contract.methods.name().call();
+      const symbol = await contract.methods.symbol().call();
+
+      console.log(`key    : ${key}`);
+      console.log(`name   : ${name}`);
+      console.log(`symbol : ${symbol}`);
+      console.log(`type   : ${value.type}`);
+      console.log(`${value.parent} => ${value.child}`);
+      console.log("------");
+      const token = {
+        key: key,
+        name: name,
+        symbol: symbol,
+        type: value.type,
+        parentContract: value.parent,
+        childContract: value.child,
+      };
+      tokens.push(token);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  res
+    .status(HTTP_STATUS_CODE.OK)
+    .json(successResponse("plasma token list fetched", { tokens }));
+};
+
+const getPos = async (req, res) => {
+  const client = await getClient("pos");
+  const parentWeb3 = client.web3Client.getParentWeb3();
+  const abi = [
+    {
+      inputs: [],
+      name: "name",
+      outputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "symbol",
+      outputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
+
+  console.log("");
+  let tokens = new Array();
+
+  for (const [key, value] of Object.entries(config.tokens.pos)) {
+    try {
+      let name;
+      let symbol;
+      if (key === "weth") {
+        name = "Wrapped Ether";
+        symbol = "WETH";
+      } else if (value.type === "erc1155") {
+        name = key;
+        symbol = "";
+      } else {
+        const contract = new parentWeb3.eth.Contract(abi, value.parent);
+        name = await contract.methods.name().call();
+        symbol = await contract.methods.symbol().call();
+      }
+      console.log(`key    : ${key}`);
+      console.log(`name   : ${name}`);
+      console.log(`symbol : ${symbol}`);
+      console.log(`type   : ${value.type}`);
+      console.log(`${value.parent} => ${value.child}`);
+      console.log("------");
+      const token = {
+        key: key,
+        name: name,
+        symbol: symbol,
+        type: value.type,
+        parentContract: value.parent,
+        childContract: value.child,
+      };
+      tokens.push(token);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  res
+    .status(HTTP_STATUS_CODE.OK)
+    .json(successResponse("pos token list fetched", { tokens }));
+};
+
 // export module
 module.exports = {
   getInfo,
+  getPlasma,
+  getPos,
 };
--- routes/api/token.js
+++ routes/api/token.js
@@ -4,5 +4,7 @@ const { errorResponse, successResponse } = require("../../util/response.js");
 const { ERROR_MESSAGE, HTTP_STATUS_CODE } = require("../../util/constants.js");
 
 router.get("/info/:tokenAddress", tokenController.getInfo);
+router.get("/list/plasma", tokenController.getPlasma);
+router.get("/list/pos", tokenController.getPos);
 
 module.exports = router;
